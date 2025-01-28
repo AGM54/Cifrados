4d9bc8df9b29ac5e742efeefd7e53cf40055c589
astridglauser88 <astridglauser8@gmail.com>
2025-01-27 23:52:34 -0600

    7
    Realizar la creación de un script que permita generar llaves dinámicas (utilizar ASCII)

---
+++ Ejercicios/ejercicio7.py
@@ -0,0 +1,24 @@
+import random
+
+def generar_llave(longitud):
+    llave = ''.join(chr(random.randint(32, 126)) for _ in range(longitud))
+    return llave
+
+def texto_a_binario(texto):
+    return ''.join(format(ord(caracter), '08b') for caracter in texto)
+
+
+texto = input("Introduce el texto para generar una llave dinámica: ")
+
+# Genera una llave de la misma longitud que el texto
+llave = generar_llave(len(texto))
+
+# Convierte el texto y la llave a binario
+texto_binario = texto_a_binario(texto)
+llave_binaria = texto_a_binario(llave)
+
+# Muestra los resultados
+print("\nTexto Original (ASCII):", texto)
+print("Texto en Binario:", texto_binario)
+print("\nLlave Generada (ASCII):", llave)
+print("Llave en Binario:", llave_binaria)
