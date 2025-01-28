6cd9acebcd7e30bf61524e62b002357e8c1e73e8
astridglauser88 <astridglauser8@gmail.com>
2025-01-27 19:04:43 -0600

    Update ejercicio2.py

--- Ejercicios/ejercicio2.py
+++ Ejercicios/ejercicio2.py
@@ -4,22 +4,16 @@ import base64
 def base64_a_binario(texto_base64):
     # Decodifica el texto Base64 a bytes
     bytes_decodificados = base64.b64decode(texto_base64)
-    resultado_binario = []
-    for byte in bytes_decodificados:
-        binario = bin(byte)[2:]  
-        while len(binario) < 8:
-            binario = '0' + binario
-        resultado_binario.append(binario)
+    # Convierte cada byte a binario de 8 bits
+    resultado_binario = [format(byte, '08b') for byte in bytes_decodificados]
     return ' '.join(resultado_binario)
 
-
+# Entrada del usuario
 texto_base64 = input("Introduce el texto en Base64 que deseas convertir a binario: ")
 binario = base64_a_binario(texto_base64)
 
 print("\nTexto en Binario:")
 print(binario)
-
-
 
 
 
